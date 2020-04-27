dbc37ebdad342cdac27eb253b90eac0590d06e76
iammiori <mioriii@naver.com>
2020-04-27 13:52:15 +0900

    [feat] Linear vertical

--- 0-layout/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
+++ 0-layout/FirstApplication/app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.firstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1 = (Button)findViewById(R.id.button1);
+        b2 = (Button)findViewById(R.id.button_cal);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LinearVertical.class);
+                startActivity(intent);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyCal.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
